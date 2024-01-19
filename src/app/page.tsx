a6bd4fd11102f9d6825ad1ccef83140aae5fb9da
XPS <evgkamok@gmail.com>
2024-01-19 19:32:07 +0300

    compete rating tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
+"use client";
+
 import Button from "@/components/common/Button/Button";
 import HeadTitleTag from "@/components/common/HeadTitleTag/HeadTitleTag";
 import Paragraph from "@/components/common/Paragraph/Paragraph";
 import Rating from "@/components/common/Rating/Rating";
 import Tag from "@/components/common/Tag/Tag";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [rating, setRating] = useState(4);
+
   return (
     <>
       <HeadTitleTag tag="h1">Hello world NEXT !</HeadTitleTag>
       <Button appearance="primary" arrow="right">Primary button</Button>
       <Button appearance="ghost" arrow="right">Ghost button</Button>
-      <Rating isEditable rating={5} />
+      <Rating isEditable rating={rating} setRating={setRating}/>
       <Paragraph>Студенты освоят не только hard skills, необходимые для работы веб-дизайнером, но и soft skills — навыки, которые позволят эффективно взаимодействовать в команде с менеджерами, разработчиками и маркетологами. Выпускники факультета могут успешно конкурировать с веб-дизайнерами уровня middle.</Paragraph>
       <Paragraph font_size="s">Напишу сразу в двух курсах, так как проходил оба. Java будет многим непросвещённым сложновата в изучении, но здесь перевес из-за лидирующего положения языка как самого популярного в программировании. Выбор мой пал на эту профессию еще и потому, что Java-разработчики получают самую большую зарплату. Хотя Python начинает догонять Java по многим моментам, но вот в крупном екоме разработке Джава все-таки остается главенствующей сейчас. Скажу так – полнота программы и интенсивность присуща обоим курсам GeekBrains. Хочу отметить, что с первого дня занятий вы приступаете к практике и получаете опыт коммерческой разработки уже в свое резюме. Скажу вам как прошедший это – реально помогло в трудоустройстве!</Paragraph>
       <Paragraph font_size="l">Выше указаны программы Adobe InDesign, Adobe Illustrator, Corel Draw и ими можно успешно пользоваться дома или в дороге. Современные ноутбуки хорошо справляются с нагрузкой, так зачем загонять специалиста в душный офис. В этой профессии важным считается вдохновение, поэтому дизайнеры ищут его в разных местах.</Paragraph>
--- src/components/common/Rating/Rating.tsx
+++ src/components/common/Rating/Rating.tsx
@@ -3,9 +3,9 @@ import RatingProps from "./Rating.props";
 import StarIcon from "../../assets/star-icon.svg";
 import styles from './Rating.module.css';
 import cn from 'classnames';
-import { useEffect, useState } from "react";
+import { KeyboardEvent, useEffect, useState } from "react";
 
-const Rating = ({rating, isEditable = false, ...props} : RatingProps ) : JSX.Element =>  {
+const Rating = ({rating, setRating, isEditable = false, ...props} : RatingProps ) : JSX.Element =>  {
 
     const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
     
@@ -16,31 +16,51 @@ const Rating = ({rating, isEditable = false, ...props} : RatingProps ) : JSX.Ele
     const updateRating = (currentRating: number) => {
         const updatedRatingArray = ratingArray.map( (elem , index)  => {
             return (
-                <StarIcon
-                key={index}
+                <span
                     className={cn(styles.star, {
                         [styles.filled]: currentRating > index,
                         [styles.editable]: isEditable
                     })}
-                onMouseEnter={() => changeRating(index + 1)}
-                onMouseLeave={() => changeRating(rating)}
-                />
+                    key={index}
+                    onMouseEnter={() => changeRatingMouse(index + 1)}
+                    onMouseLeave={() => changeRatingMouse(rating)}
+                    onClick={() => changeRatingClick(index + 1)}
+                >
+                    <StarIcon
+                        tabIndex={ isEditable ? 0 : -1}
+                        onKeyPress={ (e: KeyboardEvent<SVGElement>) => handleSpace(e, index + 1)}
+                    />
+                </span>
             )
         })
         setRatingArray(updatedRatingArray)
     }
 
-    const changeRating = (i: number) => {
+    const changeRatingMouse = (i: number) => {
         if (!isEditable) {
             return
         }
         updateRating(i)
     }
 
+    const changeRatingClick = (i: number) => {
+        if (!isEditable || !setRating) {
+            return
+        }
+        setRating(i)
+    }
+
+    const handleSpace = (e: KeyboardEvent<SVGElement>, i: number) => {
+        if (e.code != 'Space' || !setRating) {
+            return
+        }
+        setRating(i)
+    }
+
     
     return (
         <div {...props}>
-            {ratingArray.map((e, i) => <span key={i}> {e} </span>)}
+            {ratingArray.map((e, i) => <span key={i}> { e } </span>)}
         </div>
     )
 }
